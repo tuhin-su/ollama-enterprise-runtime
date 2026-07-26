801843b40fa077f1d0b984cbff063a205b6429b7
tuhin-su <tuhinsubhraghara29195@gmail.com>
2026-07-26 10:21:22 +0000

    cleanup

--- integration/reg_required_test.go
+++
@@ -1,9 +0,0 @@
-//go:build integration && !fast && !release && !library && !imagegen
-
-package integration
-
-import "testing"
-
-func TestIntegrationRequiresScope(t *testing.T) {
-	t.Fatal("integration tests require one of the fast, release, or library tags")
-}
